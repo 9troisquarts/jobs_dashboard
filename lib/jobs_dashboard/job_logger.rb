d01618dd49a15b685d7e7686e69b91f7bbd815df
Patryk Warchol <patrykwarchol@MacBook-Pro-de-Patryk-2.local>
2022-01-28 10:51:29 +0100

    fix: mysql connection pool

--- lib/jobs_dashboard/job_logger.rb
+++ lib/jobs_dashboard/job_logger.rb
@@ -5,25 +5,44 @@ module JobsDashboard
     def call(item, queue)
       start = ::Process.clock_gettime(::Process::CLOCK_MONOTONIC)
       @logger.info("start")
-      @job_log = JobsDashboard::JobLog.create!(
-        sidekiq_jid: item['jid'],
-        status: 'queued',
-        item_type: item['class'],
-        args: item['args'],
-        retry: item['retry'],
-        queue: item['queue']
-      )
+
+      begin
+        ActiveRecord::Base.connection_pool.with_connection do
+          @job_log = JobsDashboard::JobLog.create!(
+            sidekiq_jid: item['jid'],
+            status: 'queued',
+            item_type: item['class'],
+            args: item['args'],
+            retry: item['retry'],
+            queue: item['queue']
+          )
+        end
+      ensure
+        ActiveRecord::Base.clear_active_connections!
+      end
 
       yield
 
       with_elapsed_time_context(start) do
         @logger.info("done")
-        @job_log.update(status: 'complete', finished_at: Time.now) if @job_log
+        begin
+          ActiveRecord::Base.connection_pool.with_connection do
+            @job_log.update(status: 'complete', finished_at: Time.now) if @job_log
+          end
+        ensure
+          ActiveRecord::Base.clear_active_connections!
+        end
       end
     rescue Exception => e
       with_elapsed_time_context(start) do
         @logger.info("fail")
-        @job_log.update(status: 'failed', finished_at: Time.now, backtrace: ([e.message] + e.backtrace)) if @job_log
+        begin
+          ActiveRecord::Base.connection_pool.with_connection do
+            @job_log.update(status: 'failed', finished_at: Time.now, backtrace: ([e.message] + e.backtrace)) if @job_log
+          end
+        ensure
+          ActiveRecord::Base.clear_active_connections!
+        end
       end
 
       raise
